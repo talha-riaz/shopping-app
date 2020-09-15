4850a06f03b523c14822c0e41d54dce6b4380351
talha-riaz <talha.riaz@mail.mcgill.com>
2020-09-14 23:01:42 -0400

    added store

---
+++ src/Checkout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useStateValue} from './StateProvider';
+
+function Checkout() {
+
+    const [{basket}, dispatch] = useStateValue();
+
+    return (
+        <div className='checkout'>
+            <img 
+            className="checkout__ad"
+            src="https://wordstream-files-prod.s3.amazonaws.com/s3fs-public/styles/simple_image/public/images/media/images/banner-ads-examples-aws.jpg?jwAuTeoLXQvDzDNGQ8Q3zDmWXndEqL8V&itok=tukEXkYJ"
+            alt=""
+            />
+        </div>
+    )
+}
+
+export default Checkout
--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,7 +1,19 @@
+import React, {createContext, useContext, useReducer} from 'react';
+import {reducer, initialState} from './reducer';
 //Data layer
 //Needed to track the basket
+
 
 //This is the data layer
  export const StateContext = createContext();
 
  //Build a provider
+export const StateProvider = ({reducer, initialState, children}) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {/* This 'children' refers to the app */}
+        {children}
+    </StateContext.Provider>
+)
+
+//This is how we use it inside of a component
+export const useStateValue = () => useContext(StateContext);
